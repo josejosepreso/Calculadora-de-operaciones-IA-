16fc3ad84e79e639e9fd3ed41a899b4c2c472291
josejosepreso <josehihat@gmail.com>
2025-04-09 10:43:42 -0600

    script

--- traning_data_generator/main.py
+++ traning_data_generator/main.py
@@ -12,19 +12,16 @@ n = 1000
 numbers = [ f"numbers/{i}" for i in os.listdir("numbers") ]
 symbols = [ f"symbols/{i}" for i in os.listdir("symbols") ]
 
-first = []
-second = []
-
 for _ in range(n):
     first = []
     second = []
     
-    for _ in range(random.randint(1,2)):
+    for _ in range(random.randint(1,3)):
         first.append(random.choice(numbers))
     
     op = random.choice(symbols)
     
-    for _ in range(random.randint(1,2)):
+    for _ in range(random.randint(1,3)):
         second.append(random.choice(numbers))
     
     filename = f"img_{next_json_value}.png"
@@ -45,18 +42,15 @@ for _ in range(n):
     
     operacion = f"{s0}{c}{s2}"
     
-    sub1 = s0
-    sub2 = s2
-    
     if s0[0] == '0' and len(s0) > 1:
-        sub1 = f"{s0[1]}"
+        s0 = f"{s0[1]}"
     if s2[0] == '0' and len(s2) > 1:
-        sub2 = f"{s2[1]}"
+        s2 = f"{s2[1]}"
 
-    if c == '/' and sub2 == "0":
+    if c == '/' and s2 == "0":
         continue
     
-    resultado = eval(f"{sub1}{c}{sub2}")
+    resultado = eval(f"{s0}{c}{s2}")
     
     datos.append({
         "imagen": filename,
